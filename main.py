915a7a98638668de1bc919b403489501eb68d512
JUND Guy <gjund@free.fr>
2025-09-16 07:43:09 +0200

    Web Scraping 2.1

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-# 1. Création du dictionnaire fruits 
-fruits = {"pomme": "rouge", "banane": "jaune", "orange": "orange"} 
+nombre1 = input("Entrez un nombre entier: ")
+nombre2 = input("Entrez un nombre entier: ")
 
-# 2. Ajout de la clé "kiwi" 
-fruits["kiwi"] = "vert"
+# isnumeric() permet de vérifier si la chaîne de caractères est un nombre
+if not nombre1.isnumeric() or not nombre2.isnumeric():
+    print("Erreur: les deux nombres doivent être des nombres entiers")
+    raise SystemExit("Fin du programme")
 
- # 3. Accès à la valeur correspondant à la clé "banane" 
-couleur_banane = fruits["banane"] 
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
 
-# 4. Modification de la valeur associée à la clé "pomme" 
-fruits["pomme"] = "vert" 
+operation = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/']: ")
 
-# 5. Suppression de la clé "orange" 
-del fruits["banane"] 
+if operation not in ["+", "-", "*", "/"]:
+    print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+    raise SystemExit("Fin du programme")
 
-# 6. Affichage des clés restantes dans le dictionnaire
-print(fruits.keys())
+
+if operation == "+":
+    resultat = nombre1 + nombre2
+elif operation == "-":
+    resultat = nombre1 - nombre2
+elif operation == "*":
+    resultat = nombre1 * nombre2
+elif operation == "/":
+    # Vérifie si la variable `nombre2` n'est pas nulle pour la division
+    if nombre2 == 0:
+        print("Erreur: impossible de diviser par zéro.")
+        raise SystemExit("Fin du programme")
+
+    resultat = round(nombre1 / nombre2, 2)
+
+# Affiche le résultat
+print(f"Le résultat de l'opération est: {round(resultat, 2)}")
