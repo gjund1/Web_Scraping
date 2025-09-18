0bf6d55993c9c2d68557b08881871fbbc4121d98
JUND Guy <gjund@free.fr>
2025-09-18 08:18:59 +0200

    Web Scraping 3.1

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-# Définition de la fonction salaire_mensuel
-def salaire_mensuel(salaire_annuel):
-    return salaire_annuel / 12
+# Dans le fichier principal, on peut importer les fonctions de `operations.py`
+# à l'aide de l'instruction `import` , puis les utiliser pour effectuer des opérations.
+import operations
 
 
-# Définition de la fonction salaire_hebdomadaire
-def salaire_hebdomadaire(salaire_mensuel):
-    return salaire_mensuel / 4
-
-
-# Définition de la fonction salaire_horaire
-def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
-    return salaire_hebdomadaire / heures_travaillees
-
-
-# Demande à l'utilisateur de saisir son salaire annuel
-salaire_annuel = float(input("Entrez votre salaire annuel : "))
-# Demande à l'utilisateur de saisir le nombre d'heures travaillées par semaine
-heures_travaillees = float(
-    input("Entrez le nombre d'heures travaillées par semaine : "))
-
-# Calcul du salaire horaire
-mensuel = salaire_mensuel(salaire_annuel)
-hebdomadaire = salaire_hebdomadaire(mensuel)
-horaire = salaire_horaire(hebdomadaire, heures_travaillees)
-
-# Affichage du résultat
-print("Votre salaire horaire est de", horaire, "euros.")
+# Utilisation de la fonction addition
+resultat_addition = operations.addition(3, 5)
+print("Résultat de l'addition :", resultat_addition)
+# Utilisation de la fonction multiplication
+resultat_multiplication = operations.multiplication(8, 2)
+print("Résultat de la multiplication :", resultat_multiplication)
